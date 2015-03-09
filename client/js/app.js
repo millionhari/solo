7d55b85e9b138d3f045575115146167f6d3a0bdf
millionhari <mikemhari@gmail.com>
2015-03-09 15:34:20 -0700

    Clean up comments

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,9 @@
 
-//Youtube video
+//Youtube Video
 function getURL(){
-  var requestedURL = prompt('What is the link of your YouTube video?');
-  requestedURL = requestedURL.split('/watch?v=');
+  // var requestedURL = prompt('What is the link of your YouTube video?');
+  // requestedURL = requestedURL.split('/watch?v=');
+  requestedURL = [0,'uBENjCPS8LI'];
   return(requestedURL[1]);
 }
 
@@ -12,8 +13,7 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-//  This function creates an <iframe> (and YouTube player)
-//  after the API code downloads.
+// Create iframe when API code downloads
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -27,21 +27,22 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-// 4. The API will call this function when the video player is ready.
+// Run when video player is ready
 function onPlayerReady(event) {
+  event.target.mute();
   event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
+// Call function when player's state changes. When video is playing, state === 1
 var done = false;
 function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
+
+  // if (event.data == YT.PlayerState.PLAYING && !done) {
+    // setTimeout(stopVideo, 6000);
+    // done = true;
+  // }
 }
+
 function stopVideo() {
   player.stopVideo();
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 var express = require('express');
+var cors = require('cors');
 var WebSocket = require('ws');
 var app = express();
 
 
 //use client's index.html
+app.use(cors());
 app.use(express.static('../client'));
 
 app.get('/', function(req, res){
-	console.log(req);
+	console.log('hey')
 });
 
 app.listen(3000);

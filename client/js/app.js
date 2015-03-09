f54542c24f1868a8ebf17210ea54be93715c7bb3
millionhari <mikemhari@gmail.com>
2015-03-09 16:27:11 -0700

    Implement get function in app
    - Load firebase in scripts

--- client/js/app.js
+++ client/js/app.js
@@ -33,20 +33,38 @@ function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-// Call function when player's state changes. When video is playing, state === 1
-var done = false;
-function onPlayerStateChange(event) {
-
-  // if (event.data == YT.PlayerState.PLAYING && !done) {
-    // setTimeout(stopVideo, 6000);
-    // done = true;
-  // }
-}
-
 function stopVideo() {
   player.stopVideo();
 }
 
 function playVideo() {
   player.playVideo();
+}
+// Call function when player's state changes. When video is playing, state === 1
+var done = false;
+
+// AJAX requests
+var app = {
+  send: function(data){
+  $.ajax({
+      url: '/',
+      type: 'GET',
+      success: function(data){
+        console.log('success', data)
+      },
+      error: function(data){
+        console.log('error', data)
+      }
+    })
+  }
+}
+
+function onPlayerStateChange(event) {
+  
+
+  console.log(event);
+  // if (event.data == YT.PlayerState.PLAYING && !done) {
+    // setTimeout(stopVideo, 6000);
+    // done = true;
+  // }
 }
